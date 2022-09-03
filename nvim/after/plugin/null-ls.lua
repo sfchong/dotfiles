de0f04aeb6af83e4277807c7d0e06a037fae1e7f
Adrian Chong <adrian@adrianchongsf.com>
2022-09-03 15:31:29 +0800

    setup null-ls

---
+++ nvim/after/plugin/null-ls.lua
@@ -0,0 +1,28 @@
+local null_ls = require('null-ls')
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup({
+    sources = {
+        formatting.prettier,
+        diagnostics.eslint,
+        formatting.gofmt,
+    },
+    on_attach = function(client, bufnr)
+        -- auto-format on save
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.formatting_sync()
+                end,
+            })
+        end
+    end,
+})
