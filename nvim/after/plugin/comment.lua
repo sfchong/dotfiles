93c602139124c903ad24a82af448a7084369c4ee
Adrian Chong <adrian@adrianchongsf.com>
2022-09-03 15:31:47 +0800

    setup comment

---
+++ nvim/after/plugin/comment.lua
@@ -0,0 +1,15 @@
+require('Comment').setup({
+    toggler = {
+        ---Line-comment toggle keymap
+        line = '<leader>/',
+        ---Block-comment toggle keymap
+        block = 'gbc',
+    },
+    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+    opleader = {
+        ---Line-comment keymap
+        line = '<leader>/',
+        ---Block-comment keymap
+        block = 'gb',
+    }
+})
