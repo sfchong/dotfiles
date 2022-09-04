1865b2803fbacbebbb214d5859576d979970b8a5
Adrian Chong <adrian@adrianchongsf.com>
2022-09-04 19:00:40 +0800

    configure netwr

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -30,5 +30,11 @@ vim.g.wildignorecase = true
 
 vim.opt.signcolumn="yes:1"
 
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 20
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+
+
 -- use space as leader key
 vim.g.mapleader = " "
